28ee293245fa90a8b8bf0d08e67d01683b79b41a
someshium <someshjdh2018@gmail.com>
2021-06-16 18:46:27 +0530

    start

---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+import './Sidebar.css';
+import { Avatar } from '@material-ui/core';
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+
+            </div>
+
+            <div className="sidebar__search">
+
+            </div>
+
+            <div className="sidebar__chats">
+
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Sidebar
+
